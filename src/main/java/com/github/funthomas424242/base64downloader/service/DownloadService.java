20b019a3e6da6690efa2b735a5048cadc5a242e8
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-02-24 21:26:54 +0100

    Download Endpoint und Info Endpoint aufgenommen
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

---
+++ src/main/java/com/github/funthomas424242/base64downloader/service/DownloadService.java
@@ -0,0 +1,7 @@
+package com.github.funthomas424242.base64downloader.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DownloadService {
+}
--- src/main/java/com/github/funthomas424242/base64downloader/web/Base64DownloadController.java
+++ src/main/java/com/github/funthomas424242/base64downloader/web/Base64DownloadController.java
@@ -1,12 +1,29 @@
 package com.github.funthomas424242.base64downloader.web;
 
+import com.github.funthomas424242.base64downloader.service.DownloadService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.net.URL;
 
 @RestController
 public class Base64DownloadController {
-    @GetMapping("/")
-    public String getResource(){
-        return "Hello, World!!";
+
+    @Inject
+    protected DownloadService downloadService;
+
+    @GetMapping("/getInfo")
+    public String getInfo(){
+        return "Der Service ist: "+downloadService;
+    }
+
+    @GetMapping(path = "/getResource/{url}", produces= MediaType.TEXT_PLAIN_VALUE)
+    public String getResource(@PathVariable URL url){
+
+
+        return "Der Download URL ist: "+url;
     }
 }
