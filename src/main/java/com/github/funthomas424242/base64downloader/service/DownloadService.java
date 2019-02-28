385f53fae994fec52b70da50ddba00afc2146cec
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-02-28 23:29:48 +0100

    Build nur bis package
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/base64downloader/service/DownloadService.java
+++ src/main/java/com/github/funthomas424242/base64downloader/service/DownloadService.java
@@ -1,7 +1,39 @@
 package com.github.funthomas424242.base64downloader.service;
 
+import org.springframework.http.*;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Service
 public class DownloadService {
+
+
+    public ResponseEntity<byte[]> fetchFile(final String url) throws IOException {
+
+        System.out.println("Beginne Download von:"+url);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(
+            new ByteArrayHttpMessageConverter());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        ResponseEntity<byte[]> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET, entity, byte[].class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+//            Files.write(Paths.get("google.png"), response.getBody());
+            return response;
+        } else {
+            return null;
+        }
+
+    }
 }
--- src/main/java/com/github/funthomas424242/base64downloader/web/Base64DownloadController.java
+++ src/main/java/com/github/funthomas424242/base64downloader/web/Base64DownloadController.java
@@ -2,13 +2,13 @@ package com.github.funthomas424242.base64downloader.web;
 
 import com.github.funthomas424242.base64downloader.service.DownloadService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
-import java.net.URL;
+import java.io.IOException;
 
 @RestController
 public class Base64DownloadController {
@@ -22,9 +22,16 @@ public class Base64DownloadController {
     }
 
     @GetMapping(path = "/getResource", produces= MediaType.TEXT_PLAIN_VALUE)
-    public String getResource(@RequestParam String url){
+    public String getResource(@RequestParam String url) throws IOException {
+//
+//        final ResponseEntity<byte[]> entity = downloadService.fetchFile(url);
+//        entity.getBody();
+//        return entity.getBody().toString();
+        final ResponseEntity<byte[]> entity = downloadService.fetchFile(url);
+        final byte[] content = entity.getBody();
+        final String text = new String(content);
+//        final String text = "Hallo Du da";
+        return text;
 
-
-        return "Der Download URL ist: "+url;
     }
 }
